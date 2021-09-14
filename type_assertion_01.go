f1a32f3aef878732cd6ffab05506cbc290a083f2
virtualsafety <virtualsafety@users.noreply.github.com>
2021-09-14 19:00:47 +0800

    Create type_assertion_01.go

---
+++ type_assertion_01.go
@@ -0,0 +1,22 @@
+//https://yourbasic.org/golang/type-assertion-switch/
+//https://golang.org/ref/spec#Type_assertions
+
+package main
+import ( 
+    "fmt"   
+)
+
+func main() {
+	var x interface{} = "foo"
+
+	switch v := x.(type) {
+	case nil:
+		fmt.Println("x is nil")            // here v has type interface{}
+	case int: 
+		fmt.Println("x is", v)             // here v has type int
+	case bool, string:
+		fmt.Println("x is bool or string") // here v has type interface{}
+	default:
+		fmt.Println("type unknown")        // here v has type interface{}
+	}
+}
